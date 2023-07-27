476a8083c724eff1502f33fedf3ccf9989792675
IgorOdaryuk <odaryukigor@gmail.com>
2023-07-27 15:27:52 +0300

    starting the new project

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Igor Odaryuk on 27.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
